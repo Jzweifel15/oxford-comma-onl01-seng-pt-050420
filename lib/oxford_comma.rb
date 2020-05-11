bb1dfd75f91c86ec75c78ed6b64f427c2a55d676
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-11 16:38:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,12 @@
 def oxford_comma(array)
-  array.join(" and ")
+  if array.length == 2 
+    array[-2].push(" and ")
+    array.join
+  elsif array.length == 1 
+    array.join
+  elsif array.length > 2 
+    array[1].prepend "and "
+    array.join(", ")
+  end
 end
 
